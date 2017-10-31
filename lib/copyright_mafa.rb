60068730d120555708fe49fbb2c90cd91c2b7745
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2017-10-31 13:34:51 +0400

    Copyright symbol with company name

--- lib/copyright_mafa.rb
+++ lib/copyright_mafa.rb
@@ -1,15 +1,20 @@
 require "copyright_mafa/version"
 
 module CopyrightMafa
-  def copyright(start_year)
+  def copyright(start_year, company_name = nil)
     start_year = start_year.to_i
     current_year = Time.new.year
-    if current_year > start_year && start_year > 2000
+    years = if current_year > start_year && start_year > 2000
       "#{start_year} - #{current_year}"
     elsif start_year > 2000
       start_year.to_s
     else
       current_year.to_s
+    end
+    if company_name
+      "&copy; #{company_name} #{years}".html_safe
+    else
+      years
     end
   end
 end
